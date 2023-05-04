39b5965bf580c9e2206d0bfa858413ecddd9fe21
梦丶随心飞 <420443292@qq.com>
2023-05-04 22:18:31 +0800

    Add files via upload

---
+++ userinfo.py
@@ -0,0 +1,6 @@
+# This script was created by dreamfly
+# author: dreamfly
+# github: https://github.com/dfvips
+cookie = '' # 填写自己的cookie，不可留空
+fd = '' # 填写自己的fd, 可留空
+GtToken = '' # 填写自己的GtToken, 可留空
